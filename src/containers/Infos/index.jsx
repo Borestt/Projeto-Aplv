6c1da795925f4f2ea8b4492b90db1405e6ed7216
ian <ianzrocha@gmail.com>
2025-10-16 21:34:16 -0300

    Adicionado a seção de Informações

--- src/containers/Infos/index.jsx
+++ src/containers/Infos/index.jsx
@@ -1,10 +1,43 @@
-function Create() {
+import './styles.css'
+
+function Infos() {
   return (
-    <div>
-      <h1>Página de Criação</h1>
-      <p>Esta é a tela de Informações.</p>
+    <div className="infos-container">
+      <h1>Página de Informação</h1>
+
+      <p>
+        Bem-vindo ao nosso mundo de RPG online, um universo onde estratégia,
+        imaginação e aventura se encontram!
+      </p>
+
+      <p>
+        Aqui, você cria o seu próprio personagem e escolhe entre diversas
+        classes como Guerreiro, Mago e Guardião.
+      </p>
+
+      <p>
+        Embarque em batalhas épicas contra criaturas sombrias que ameaçam a
+        paz do reino. Enfrente monstros lendários e chefes (BOSS) — inimigos
+        colossais que exigem coragem para serem derrotados.
+      </p>
+
+      <p>
+        Suba de nível, conquiste moedas e equipamentos enquanto avança. Cada
+        missão e cada combate moldam sua trajetória e tornam seu herói mais
+        poderoso.
+      </p>
+
+      <p>
+        Participe de eventos, descubra segredos escondidos e ganhe recompensas
+        raras. Seu destino está em suas mãos.
+      </p>
+
+      <section className="infos-extra">
+        <h2>Fase Beta</h2>
+        <p>o Jogo está em fase Beta, então pode apresentar problemas ou vim upgrades melhores.</p>
+      </section>
     </div>
   );
 }
 
-export default Create;
+export default Infos;
